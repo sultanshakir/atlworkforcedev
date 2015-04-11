0077f7eb210847cb6e6616aeb9f86ae933586436
jrec15 <jrec15@uga.edu>
2015-04-11 19:49:13 -0400

    added two buttons to home. was playing around with materialize/font awesome

--- public/app/views/welcome/home.php
+++ public/app/views/welcome/home.php
@@ -6,3 +6,20 @@
  * Time: 8:08 PM
  */
 ?>
+<div>
+<!--    <img id='suit-img' class="responsive-img" src="../../../documentation/assets/images/suit.jpg">-->
+    <div class="row">
+        <div class="col s12">
+            <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
+                <i class="fa fa-chevron-right fa-fw"></i>&nbsp;&nbsp;Donate Suit
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
+                <i class="fa fa-chevron-right fa-3x"></i>&nbsp;&nbsp;Get Suit
+            </a>
+        </div>
+    </div>
+</div>
