849687d11e3f30501a13b8adee61d27660c2de94
matt trask <mjftrask@gmail.com>
2015-04-11 21:01:57 -0400

    fixed button formatting.

--- public/app/views/welcome/home.php
+++ public/app/views/welcome/home.php
@@ -15,13 +15,14 @@
             <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
                 <i class="fa fa-chevron-right fa-fw"></i>&nbsp;&nbsp;Donate Suit
             </a>
+            <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
+                <i class="fa fa-chevron-right fa-3x"></i>&nbsp;&nbsp;Get Suit
+            </a>
         </div>
     </div>
     <div class="row">
         <div class="col s12">
-            <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
-                <i class="fa fa-chevron-right fa-3x"></i>&nbsp;&nbsp;Get Suit
-            </a>
+
         </div>
     </div>
 </div>
