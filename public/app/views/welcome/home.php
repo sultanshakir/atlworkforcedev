6c65c68fc221a195db88e95d6cd1e1d43fd9ca2e
matt trask <mjftrask@gmail.com>
2015-04-11 20:56:14 -0400

    fixed a merge conflict with justin's branch

--- public/app/views/welcome/home.php
+++ public/app/views/welcome/home.php
@@ -7,6 +7,22 @@
  */
 ?>
 
-<div class="main" id="home">
 
+<div id="home" class="main">
+    <!--    <img id='suit-img' class="responsive-img" src="../../../documentation/assets/images/suit.jpg">-->
+    <div class="row">
+        <div class="col s12">
+            <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
+                <i class="fa fa-chevron-right fa-fw"></i>&nbsp;&nbsp;Donate Suit
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
+                <i class="fa fa-chevron-right fa-3x"></i>&nbsp;&nbsp;Get Suit
+            </a>
+        </div>
+    </div>
 </div>
+
