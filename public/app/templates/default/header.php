fe3cc4357e02b0f352b59d9bda7399d63493af3b
matt trask <mjftrask@gmail.com>
2015-04-12 20:15:01 -0400

    added link paths to at the nav bar

--- public/app/templates/default/header.php
+++ public/app/templates/default/header.php
@@ -24,13 +24,13 @@
 
 	<nav id="nav">
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo">SuitSwap</a>
+			<a href="home" class="brand-logo">SuitSwap</a>
 			<a href="#" class="button-collapse" data-activates="mobile-demo"><i class="mdi-navigation-menu"></i></a>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a href="#">About Us</a></li>
-				<li><a href="#">Services</a></li>
-				<li><a href="#">Donate</a></li>
-				<li><a href="#">Contact Us</a></li>
+				<li><a href="about">About Us</a></li>
+				<li><a href="services">Services</a></li>
+				<li><a href="donate">Donate</a></li>
+				<li><a href="contact">Contact Us</a></li>
 			</ul>
 		</div>
 	</nav>
