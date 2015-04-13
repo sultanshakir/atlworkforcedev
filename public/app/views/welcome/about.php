396f86c4f5c690a22dac6f13940291419e83a6cb
matt trask <mjftrask@gmail.com>
2015-04-12 20:15:13 -0400

    building up about us page

--- public/app/views/welcome/about.php
+++ public/app/views/welcome/about.php
@@ -9,6 +9,13 @@
 
 <div class="main" id="about">
 
-    <h1>Echo World</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h1>Who are we?</h1>
+                <p>We are an arm of Atlanta Workforce Development Agency. The Atlanta Workforce Development Agency (AWDA) serves as the workforce system for the City of Atlanta. The agency will provide job seekers with resources to attain sustainable employment and collaborate with business leaders for the recruitment and development of their labor needs</p>
+            </div>
+        </div>
+    </div>
 
 </div>
