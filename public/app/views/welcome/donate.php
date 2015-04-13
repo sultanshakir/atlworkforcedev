610df10248273bf82d003876d6adf421c7108686
jrec15 <jrec15@uga.edu>
2015-04-13 08:55:24 -0400

    added donate page

---
+++ public/app/views/welcome/donate.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joey
+ * Date: 4/12/15
+ * Time: 9:31 PM
+ */
+?>
+
+<div class="main" id="donate">
+
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h1 id="header">We are happy to accept the following donations</h1>
+                <ul class="collection with-header">
+                    <li class="collection-header"><h4>We have a great need for:</h4></li>
+                    <li class="collection-item">Small suits</li>
+                    <li class="collection-item">Ties</li>
+                    <li class="collection-item">Women's professional clothes</li>
+                </ul>
+                <ul class="collection with-header">
+                    <li class="collection-header"><h4>We also accept the following:</h4></li>
+                    <li class="collection-item">Large suits</li>
+                </ul>
+                <h2>Thank you for you support!</h2>
+            </div>
+        </div>
+    </div>
+
+</div>
