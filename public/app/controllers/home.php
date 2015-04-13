9dffe1953c08d8fe78ac0d230233a9c632ef025f
jrec15 <jrec15@uga.edu>
2015-04-13 08:58:36 -0400

    linking for donate page

--- public/app/controllers/home.php
+++ public/app/controllers/home.php
@@ -13,31 +13,32 @@ use core\view;
 class Home extends \core\controller {
 
     public function index() {
-
         View::rendertemplate('header');
         View::render('welcome/home');
         View::rendertemplate('footer');
     }
 
     public function about() {
-
-
         View::rendertemplate('header');
         View::render('welcome/about');
         View::rendertemplate('footer');
     }
 
     public function services() {
-
         View::rendertemplate('header');
         View::render('welcome/services');
         View::rendertemplate('footer');
     }
 
     public function contact() {
-
         View::rendertemplate('header');
         View::render('welcome/contact');
+        View::rendertemplate('footer');
+    }
+
+    public function donate() {
+        View::rendertemplate('header');
+        View::render('welcome/donate');
         View::rendertemplate('footer');
     }
 }
