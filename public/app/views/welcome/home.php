495640cd9dc2b6dbe3ac374fb1fe6c3bc03bb239
matt trask <mjftrask@gmail.com>
2015-04-12 21:44:45 -0400

    button formatting fix and added some height

--- public/app/views/welcome/home.php
+++ public/app/views/welcome/home.php
@@ -10,11 +10,16 @@
 
 <div id="home" class="main">
     <!--    <img id='suit-img' class="responsive-img" src="../../../documentation/assets/images/suit.jpg">-->
+    <div style="height: 400px">
+
+    </div>
     <div class="row">
-        <div class="col s12">
+        <div class="col s12 m6 l6">
             <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
                 <i class="fa fa-chevron-right fa-fw"></i>&nbsp;&nbsp;Donate A Suit
             </a>
+        </div>
+        <div class="col s12 m6 l6">
             <a class="waves-effect waves-light teal lighten-3 btn-large suit-btn-large">
                 <i class="fa fa-chevron-right fa-3x"></i>&nbsp;&nbsp;Get A Suit
             </a>
